637f2ef110048f7741f0a113543220df2e0d251b
Alex Manaila <amanaila@oakland.edu>
2020-03-20 14:12:44 -0400

    :)

---
+++ Source/PC/src/main/java/com/syminical/ANT_PC/AntPcApplication.java
@@ -0,0 +1,13 @@
+package com.syminical.ANT_PC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntPcApplication.class, args);
+	}
+
+}
---
+++ Source/PC/src/test/java/com/syminical/ANT_PC/AntPcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syminical.ANT_PC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
