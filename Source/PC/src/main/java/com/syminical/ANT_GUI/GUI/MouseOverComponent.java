f4fa3d78d412c81c6ec9c3e12baea98b1a84dfa3
Alex Manaila <amanaila@oakland.edu>
2020-04-06 23:03:59 -0400

    component's background changes based on mouse presence

--- Source/PC/src/main/java/com/syminical/ANT_GUI/GUI/MouseOverComponent.java
+++ Source/PC/src/main/java/com/syminical/ANT_GUI/GUI/MouseOverComponent.java
@@ -1,12 +1,25 @@
 package syminical.ant_gui;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.*;
 import javax.swing.*;
 
-public class MouseOverComponent extends BackgroundPanel {
-   //
-   public InteractiveComponent(Image[] States) {
-      
+public class MouseOverComponent extends BackgroundPanel implements MouseListener {
+   private final int INACTIVE = 0, ACTIVE = 1;
+   private Image[] States;
+
+   public MouseOverComponent(Image[] __) {
+      super(__[0]);
+      States = __;
+      addMouseListener(this);
+      addMouseListener(ANT_GUI.getDL());
+      addMouseMotionListener(ANT_GUI.getDL());
    }
+   
+   public void mouseEntered(MouseEvent ME) { setImage(States[ACTIVE]); }
+   public void mouseExited(MouseEvent ME) { setImage(States[INACTIVE]); }
+   public void mouseClicked(MouseEvent ME) { }
+   public void mousePressed(MouseEvent ME) { }
+   public void mouseReleased(MouseEvent ME) { }
 }
