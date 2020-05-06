d7e2d6893aa202a8f4c39a4a89b627904a8e0a3b
syminical <syminical@gmail.com>
2020-05-06 17:15:33 -0400

    accessible data from Model

--- Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/ModelData.java
+++ Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/ModelData.java
@@ -1,5 +1,10 @@
 package syminical.ant_gui;
 
 public enum ModelData {
+//IMAGES
+   FrameAssets, ConnectionAssets;
    
+   /*private final int id;
+   private ModelData(int __) { this.id = __; }
+   public int id() { return id; }*/
 }
