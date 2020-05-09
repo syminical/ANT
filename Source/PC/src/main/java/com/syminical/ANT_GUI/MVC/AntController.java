3adb0feae3cd80ef8b376bb4f7489b0b4fed389c
syminical <syminical@gmail.com>
2020-05-09 17:22:15 -0400

    frame & navBar controls exposed, splash animation

--- Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/AntController.java
+++ Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/AntController.java
@@ -3,16 +3,31 @@ package syminical.ant_gui;
 public class AntController {
    private static DragListener DL;
 
-   public static void frameCloseXButton() {
+   public static void frameCloseX() {
       if (ANT_GUI.View() != null) ANT_GUI.View().kill();
       System.exit(0);
    }
    
+   public static void navBarBack() { }
+   public static void navBarClearAll() { }
+   public static void navBarGear() { }
+   public static void navBarInfo() { }
+   public static void navBarNotifs() { }
+   public static void navBarTexts() { }
+   
    public static void initWindowListener() {
       DL = new DragListener();
          DL.setTarget(AntView.Window());
-         ANT_GUI.View().Window().addMouseListener(DL);
-         ANT_GUI.View().Window().addMouseMotionListener(DL);
+         AntView.Window().addMouseListener(DL);
+         AntView.Window().addMouseMotionListener(DL);
+   }
+   
+   public static void splashAnimation() {
+      try {
+         AntView.splash();
+         Thread.sleep(4000);
+         AntView.notifs();
+      } catch(Exception e) { System.out.println("splash animation failed"); }
    }
    
    public static DragListener DL() { return DL; }
