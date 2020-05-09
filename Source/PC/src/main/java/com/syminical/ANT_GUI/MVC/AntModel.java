10777912c48dd464c6549063f506422b1c32c993
syminical <syminical@gmail.com>
2020-05-09 17:19:42 -0400

    NavBarAssets added

--- Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/AntModel.java
+++ Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/AntModel.java
@@ -17,13 +17,30 @@ public class AntModel {
    public boolean loadAssets() {
       FrameAssets = new ImageList();
       ConnectionAssets = new ImageList();
+      NavBarAssets = new ImageList();
       
       try {
          FrameAssets.add(ImageIO.read( this.getClass().getResource("/assets/frame/SPLASH.png") ));
          FrameAssets.add(ImageIO.read( this.getClass().getResource("/assets/frame/FRAME.png") ));
-         FrameAssets.add(ImageIO.read( this.getClass().getResource("/assets/frame/closeXButton/closeXInactive.png") ));
          FrameAssets.add(ImageIO.read( this.getClass().getResource("/assets/frame/closeXButton/closeXActive.png") ));
+         FrameAssets.add(ImageIO.read( this.getClass().getResource("/assets/frame/closeXButton/closeXInactive.png") ));
          
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/backArrow/BAactive.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/backArrow/BAicon.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/clearAll/CAactive.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/clearAll/CAinactive.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/notifs/NotifsHover.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/notifs/NotifsIcon.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/notifs/NotifsActive.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/texts/TextsHover.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/texts/TextsIcon.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/texts/TextsActive.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/gear/GearHover.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/gear/GearIcon.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/gear/GearActive.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/info/InfoHover.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/info/InfoIcon.png") ));
+         NavBarAssets.add(ImageIO.read( this.getClass().getResource("/assets/navBar/info/InfoActive.png") ));
          
          ConnectionAssets.add(ImageIO.read( this.getClass().getResource("/assets/connection/options/ConOpTitle.png") ));
          ConnectionAssets.add(ImageIO.read( this.getClass().getResource("/assets/connection/options/prefs/ConOpPrefTitle.png") ));
@@ -50,6 +67,7 @@ public class AntModel {
       switch (__) {
          case FrameAssets: return FrameAssets;
          case ConnectionAssets: return ConnectionAssets;
+         case NavBarAssets: return NavBarAssets;
          default: return null;
       }
    }
--- Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/ModelData.java
+++ Source/PC/src/main/java/com/syminical/ANT_GUI/MVC/ModelData.java
@@ -2,7 +2,7 @@ package syminical.ant_gui;
 
 public enum ModelData {
 //IMAGES
-   FrameAssets, ConnectionAssets;
+   FrameAssets, NavBarAssets, ConnectionAssets;
    
    /*private final int id;
    private ModelData(int __) { this.id = __; }
