121919a8f594f0dcd17db6f1dd4bb9dfcb651bcc
syminical <syminical@gmail.com>
2020-05-22 14:33:44 -0400

    faster scrolling speed

--- Source/PC/src/main/java/com/syminical/ANT_GUI/GUI/AntScrollBar.java
+++ Source/PC/src/main/java/com/syminical/ANT_GUI/GUI/AntScrollBar.java
@@ -1,3 +1,20 @@
+//  Copyright (C) 2020 Alexandru Manaila.
+
+/*  This file is part of A.N.T.
+
+    A.N.T. is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    A.N.T. is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with A.N.T.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package syminical.ant_gui;
 
 import java.awt.*;
@@ -18,7 +35,7 @@ public class AntScrollBar extends JScrollBar implements MouseListener {
       this.addMouseListener(this);
       this.setOpaque(false);
       AntFrame.fixComponentSizes(this, new Dimension(4, 357));
-      this.setUnitIncrement(20);
+      this.setUnitIncrement(40);
       //this.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 6, 0, 6));
    }
    
